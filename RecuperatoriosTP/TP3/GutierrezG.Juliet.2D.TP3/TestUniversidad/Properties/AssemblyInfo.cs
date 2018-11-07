ea4d8cc6b96d441176e31cbb8faf3a50e9dfe421
julietAGuti <juliethaguti@gmail.com>
2018-11-07 13:22:34 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP3/GutierrezG.Juliet.2D.TP3/TestUniversidad/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUniversidad")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUniversidad")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7560bdb5-295e-4ccb-8a1f-5606fcbb5087")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RecuperatoriosTP/TP3/GutierrezG.Juliet.2D.TP3/TestUniversidad/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesInstanciables;
+using EntidadesAbstractas;
+using Excepciones;
+
+namespace TestUniversidad
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DniInvalidoExceptionCargarDniIntInvalido()
+        {
+            Universidad gim = new Universidad();
+            try
+            {
+                Alumno a2 = new Alumno(2, "Juana", "Martinez", "92234458",
+                EntidadesAbstractas.Persona.ENacionalidad.Argentino, Universidad.EClases.SPD,
+                Alumno.EEstadoCuenta.AlDia);
+                gim += a2;
+            }
+            catch (NacionalidadInvalidaException e)
+            {
+                Assert.IsInstanceOfType(e, typeof(NacionalidadInvalidaException));
+            }
+        }
+
+        [TestMethod]
+        public void SinProfesorExceptionCargarSinProfesor()
+        {
+            Universidad gim = new Universidad();
+            try
+            {
+                gim += Universidad.EClases.SPD;
+            }
+            catch (SinProfesorException e)
+            {
+                Assert.IsInstanceOfType(e, typeof(SinProfesorException));
+            }
+        }
+
+        [TestMethod]
+        public void ValidarValorNumerico()
+        {
+            Profesor p = new Profesor(1, "Julia", "Zuluaga", "98988733", Persona.ENacionalidad.Extranjero);
+            Assert.IsInstanceOfType(p.DNI, typeof(int));
+        }
+
+        [TestMethod]
+        public void ValidarLetrasEnNombres()
+        {
+            Alumno a = new Alumno(4, "Çesar", "Galvan", "84756123", Persona.ENacionalidad.Argentino,
+                Universidad.EClases.Legislacion);
+            Assert.IsNotNull(a.Nombre);
+        }
+    }
+}
