ddcf44bc113076214f1bcc10796a84abc2d8019e
julietAGuti <juliethaguti@gmail.com>
2018-11-11 22:16:59 -0300

    Add files via upload

---
+++ TP4/TestUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25f9475b-8054-47cb-88c4-a04eeaaac42d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TP4/TestUnitarios/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using Excepciones;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestListaDePaquetesInstanciada()
+        {
+            Correo c = new Correo();
+            Assert.IsNotNull(c.Paquetes);
+        }
+
+        [TestMethod]
+        public void TestCargaDePaquetesAUnSoloTracking()
+        {
+            Correo c = new Correo();
+            Paquete p1 = new Paquete("Direccion", "0000000000");
+            Paquete p2 = new Paquete("Direccion2", "0000000000");
+            try
+            {
+                c += p1;
+                c += p2;
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(TrackingIdRepetidoException));
+            }
+        }
+    }
+}
